26d5886e2d902c27c3605b6ab351d5fc6e7141bf
юля <@WIN-TDMI2IAM32L>
2022-09-27 21:33:12 +0300

    Добавьте файлы проекта.

---
+++ RegVir/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegVir
+{
+    internal class Program : Raspisanie
+    {
+        static void Main(string[] args)
+        {
+
+            Raspisanie cl = new Raspisanie();
+            cl.raspisanie();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegVir/Raspisanie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegVir
+{
+    internal class Raspisanie
+    {
+        public void raspisanie()
+        {
+            string s = "Утром: прес качат 07:00, бегит 07:30, турник 09:00, анжуманя 10:00. " +
+                    "Вечером: прес качат 18:00, бегит 18:54, турник 25:03, анжуманя 00:00.";
+            Regex regex = new Regex(@"\b(?:[01][0-9]|2[0-3]):[0-5][0-9]\b");
+            MatchCollection matches = regex.Matches(s);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
